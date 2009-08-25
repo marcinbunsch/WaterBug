67e9f8387b866289717fa32e7c911b2ea305daf5
Marcin Bunsch <marcin@applicake.com>
2009-08-25 17:30:30 +0200

    bundu in da house

--- waterbug.js
+++ waterbug.js
@@ -1,25 +1,55 @@
 WaterBug = new Object();
+WaterBug.Adapters = new Object()
+// Prototype adapter
+WaterBug.Adapters.Prototype = {
+  get: function(id) { return $(id) },
+  create: function(tag_name, attr) { return new Element(tag_name, attr) }
+}
+// jQuery adapter
+WaterBug.Adapters.jQuery = {
+  $: function(selector) { return jQuery(selector) },
+  $$: function(selector) { return jQuery(selector) },
+  get: function(id) { return document.getElementById(id) },
+  create: function(tag_name, attr) { 
+    var element = document.createElement(tag_name);
+    $.each(attr, function(key, value) {
+      $(element).attr(key, value)
+    });
+    element.insert = function(content) { $(this).append(content) }
+    element.remove = function(content) { $(this).remove() }
+    return element
+  }
+}
+WaterBug.adapter = function() {
+  if (typeof(Prototype) != 'undefined') return WaterBug.Adapters.Prototype
+  if (typeof(jQuery) != 'undefined')  {
+    // Add some stuff to match Prototype and ease life a biy
+    if (!document.body.insert) { document.body.insert = function(content) { $(this).append(content) }; }
+    return WaterBug.Adapters.jQuery
+  }
+}
 WaterBug.load = function() {
   WaterBug.inspect_mode = 0;
-
-  WaterBug.display_wrapper = new Element('div', {id: 'water_bug_display_wrapper', style:"position:absolute; bottom: 0px; left: 0px; width: 100%; height: 300px; background-color: #000; padding: 0px; z-index: 1000;"});
-  WaterBug.display1 = new Element('div', {style:"width: 550px; margin: 10px; background-color: #FFF; overflow: scroll; height: 260px; padding: 10px; overflow: hidden; float: left;"});
-  WaterBug.display2 = new Element('div', {style:"width: 550px; margin: 10px; background-color: #FFF; overflow: scroll; height: 260px; padding: 10px; overflow: hidden; float: left;"});
-  WaterBug.display3 = new Element('div', {style:"display: none; clear: both;"});
-  WaterBug.close_link = new Element('a', {style:"width: 40px; height: 20px; color: #FFF; position: absolute; top: 1px; right: 1px; font-size: 7px; cursor: pointer;"});
-  WaterBug.hide_link = new Element('a', {style:"width: 40px; height: 20px; color: #FFF; position: absolute; top: 1px; right: 25px; font-size: 7px; cursor: pointer;"});
-  WaterBug.label = new Element('p', {style:"width: 100px; height: 20px; color: #FFF; position: absolute; top: 1px; right: 50%; font-size: 7px;"});
+  // TODO: find a fix for position: fixed in IE 6
+  WaterBug.display_wrapper = WaterBug.adapter().create('div', {id: 'water_bug_display_wrapper', style:"position: fixed; bottom: 0px; left: 0px; width: 100%; height: 330px; background-color: #000; padding: 0px; z-index: 1000;"});
+  WaterBug.header = WaterBug.adapter().create('div', {id: 'water_bug_display_header', style:"background-color: #333; height: 20px;"});
+  WaterBug.display1 = WaterBug.adapter().create('div', {style:"width: 550px; margin: 10px; background-color: #FFF; overflow: scroll; height: 260px; padding: 10px; overflow: hidden; float: left;"});
+  WaterBug.display2 = WaterBug.adapter().create('div', {style:"width: 550px; margin: 10px; background-color: #FFF; overflow: scroll; height: 260px; padding: 10px; overflow: hidden; float: left;"});
+  WaterBug.display3 = WaterBug.adapter().create('div', {style:"display: none; clear: both;"});
+  WaterBug.close_link = WaterBug.adapter().create('a', {style:"width: 40px; height: 20px; color: #FFF; position: absolute; top: 1px; right: 15px; font-size: 12px; cursor: pointer;"});
+  WaterBug.hide_link = WaterBug.adapter().create('a', {style:"width: 40px; height: 20px; color: #FFF; position: absolute; top: 1px; right: 15px; font-size: 12px; cursor: pointer;"});
+  WaterBug.label = WaterBug.adapter().create('a', {style:"width: 100px; height: 20px; color: #FFF; position: absolute; top: 1px; left: 15px; font-size: 12px;", href: 'http://applicake.com/'});
   WaterBug.close_link.innerHTML='X close';
   WaterBug.hide_link.innerHTML='_ hide';
   WaterBug.label.innerHTML='WaterBug';
   WaterBug.close_link.onclick = function(){WaterBug.unload();};
   WaterBug.hide_link.onclick = function(){WaterBug.toggle_visibility();};
-  WaterBug.label.onclick = function(){window.loaction="http://applicake.com/";};
+  WaterBug.display_wrapper.insert(WaterBug.header, {position:'bottom'});
   WaterBug.display_wrapper.insert(WaterBug.display1, {position:'bottom'});
   WaterBug.display_wrapper.insert(WaterBug.display2, {position:'bottom'});
   WaterBug.display_wrapper.insert(WaterBug.display3, {position:'bottom'});
   WaterBug.display_wrapper.insert(WaterBug.hide_link, {position:'bottom'});
-  WaterBug.display_wrapper.insert(WaterBug.close_link, {position:'bottom'});
+ // WaterBug.display_wrapper.insert(WaterBug.close_link, {position:'bottom'});
   WaterBug.display_wrapper.insert(WaterBug.label, {position:'bottom'});
   document.body.insert(WaterBug.display_wrapper, {position:'bottom'});
   WaterBug.display1.innerHTML = '<textarea id="water_bug_command_history" style="width: 400px; height: 200px;"></textarea><input type="text" id="water_bug_command_line" style="width: 300px;" /><input type="submit" id="water_bug_command_button" />';
@@ -27,18 +57,21 @@ WaterBug.load = function() {
 
   /******* initialize the cool stuff *******/
 
-  WaterBug.command_button = $('water_bug_command_button');
-  WaterBug.command_line = $('water_bug_command_line');
-  WaterBug.command_history = $('water_bug_command_history');
-  WaterBug.inspect_button = $('water_bug_inspect_button');
-  WaterBug.property_name_field = $('water_bug_property_name');
-  WaterBug.property_value_field = $('water_bug_property_value');
+  WaterBug.command_button = WaterBug.adapter().get('water_bug_command_button');
+  WaterBug.command_line =  WaterBug.adapter().get('water_bug_command_line');
+  WaterBug.command_history = WaterBug.adapter().get('water_bug_command_history');
+  WaterBug.inspect_button = WaterBug.adapter().get('water_bug_inspect_button');
+  WaterBug.property_name_field = WaterBug.adapter().get('water_bug_property_name');
+  WaterBug.property_value_field = WaterBug.adapter().get('water_bug_property_value');
   WaterBug.command_button.onclick = WaterBug.run;
   WaterBug.inspect_button.onclick = WaterBug.toggle_inspect_mode;
   WaterBug.property_name_field.onchange = WaterBug.inspect_property;
   WaterBug.property_name_field.onkeyup =  WaterBug.inspect_property;
   WaterBug.property_value_field.onchange = WaterBug.set_property;
   WaterBug.property_value_field.onkeyup =  WaterBug.set_property;
+  
+  // Start minimized
+  WaterBug.display_wrapper.style.height = '20px'
 }
 WaterBug.inspect_property = function() {
   var property_name = WaterBug.property_name_field.value;
@@ -62,10 +95,12 @@ WaterBug.unload = function() {
   WaterBug.display_wrapper.remove();
 }
 WaterBug.toggle_visibility = function() {
-  if (0 + WaterBug.display_wrapper.style.height.replace("px", "") > 100)
-    WaterBug.display_wrapper.style.height = '12px';
-  else
+  if (0 + WaterBug.display_wrapper.style.height.replace("px", "") > 100) {
+    WaterBug.display_wrapper.style.height = '20px';
+    WaterBug.display_wrapper.style.overflow = 'hidden';
+  } else {
     WaterBug.display_wrapper.style.height = '300px';
+  }
 }
 WaterBug.run = function(event, command) {
   if (!command) {
@@ -82,7 +117,9 @@ WaterBug.run = function(event, command) {
   WaterBug.command_history.scrollTop=WaterBug.command_history.scrollHeight;
 }
 WaterBug.toggle_inspect_mode = function () {
-  var elements = $$('div').concat($$('p')).concat($$('input')).concat($$('span')).concat($$('a'));
+  var elements = WaterBug.adapter().$$('div');
+  alert(elements)
+  //.concat($$('p')).concat($$('input')).concat($$('span')).concat($$('a'));
   if (WaterBug.inspect_mode) { //Turn inspect mode off
     WaterBug.inspect_mode = 0;
     WaterBug.inspect_button.value="INSPECT";
@@ -138,3 +175,4 @@ WaterBug.unhighlight = function(element) {
     element.original_border = null;
   }
 }
+WaterBug.load()
